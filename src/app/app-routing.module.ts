58087211a73221966bb9c44df226e55759ae780e
Руслан <ruslan@Ruslans-MacBook-Pro.local>
2024-02-20 20:29:15 +0300

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,14 @@ import { ProductItemPageComponent } from './features/product-module/product-item
 import { Page404Component } from './features/main-module/page404/page404.component';
 
 const routes: Routes = [  
-  { path: '/tea-service/', component: MainComponent },
-  { path: '/tea-service/main', component: MainComponent },
-  { path: '/tea-service/catalog', component: CatalogComponent },
-  { path: '/tea-service/catalog/:id', component: ProductItemPageComponent },
-  { path: '/tea-service/order/:id', component: OrderComponent },
-  { path: '/tea-service/product', component: ProductComponent },
-  { path: '/tea-service/not-found', component: Page404Component },
-  { path: '/tea-service/**', redirectTo: '/not-found' },
+  { path: 'tea-service', component: MainComponent },
+  { path: 'tea-service/main', component: MainComponent },
+  { path: 'tea-service/catalog', component: CatalogComponent },
+  { path: 'tea-service/catalog/:id', component: ProductItemPageComponent },
+  { path: 'tea-service/order/:id', component: OrderComponent },
+  { path: 'tea-service/product', component: ProductComponent },
+  { path: 'tea-service/not-found', component: Page404Component },
+  { path: 'tea-service/**', redirectTo: 'tea-service/not-found' },
 ];
 
 @NgModule({
