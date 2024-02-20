d74d1823986c071c785db47074e3cd2e165c0a4b
Руслан <ruslan@Ruslans-MacBook-Pro.local>
2024-02-20 19:38:44 +0300

    path fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,14 @@ import { ProductItemPageComponent } from './features/product-module/product-item
 import { Page404Component } from './features/main-module/page404/page404.component';
 
 const routes: Routes = [  
-  { path: '', component: MainComponent },
-  { path: 'main', component: MainComponent },
-  { path: 'catalog', component: CatalogComponent },
-  { path: 'catalog/:id', component: ProductItemPageComponent },
-  { path: 'order/:id', component: OrderComponent },
-  { path: 'product', component: ProductComponent },
-  { path: 'not-found', component: Page404Component },
-  { path: '**', redirectTo: '/not-found' },
+  { path: '/tea-service/', component: MainComponent },
+  { path: '/tea-service/main', component: MainComponent },
+  { path: '/tea-service/catalog', component: CatalogComponent },
+  { path: '/tea-service/catalog/:id', component: ProductItemPageComponent },
+  { path: '/tea-service/order/:id', component: OrderComponent },
+  { path: '/tea-service/product', component: ProductComponent },
+  { path: '/tea-service/not-found', component: Page404Component },
+  { path: '/tea-service/**', redirectTo: '/not-found' },
 ];
 
 @NgModule({
